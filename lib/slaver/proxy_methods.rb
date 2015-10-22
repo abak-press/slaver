03fd757306af60549569389aedb20fd13a73fc98
Denis Korobitcin <deniskorobitcin@gmail.com>
2015-10-22 16:39:54 +0500

    Optimized double current_config call in proxy_methods

--- lib/slaver/proxy_methods.rb
+++ lib/slaver/proxy_methods.rb
@@ -14,40 +14,40 @@ module Slaver
 
     module ClassMethods
       def connection_pool_with_proxy
-        if current_config
-          connection_proxy.init_connection(self, current_config).connection_pool
+        if config = current_config
+          connection_proxy.init_connection(self, config).connection_pool
         else
           connection_pool_without_proxy
         end
       end
 
       def connection_with_proxy
-        if current_config
-          connection_proxy.init_connection(self, current_config)
+        if config = current_config
+          connection_proxy.init_connection(self, config)
         else
           (connection_pool && connection_pool.connection)
         end
       end
 
       def clear_active_connections_with_proxy!
-        if current_config
-          connection_proxy.init_connection(self, current_config).clear_active_connections!
+        if config = current_config
+          connection_proxy.init_connection(self, config).clear_active_connections!
         else
           clear_active_connections_without_proxy!
         end
       end
 
       def clear_all_connections_with_proxy!
-        if current_config
-          connection_proxy.init_connection(self, current_config).clear_all_connections!
+        if config = current_config
+          connection_proxy.init_connection(self, config).clear_all_connections!
         else
           clear_all_connections_without_proxy!
         end
       end
 
       def connected_with_proxy?
-        if current_config
-          connection_proxy.init_connection(self, current_config).connected?
+        if config = current_config
+          connection_proxy.init_connection(self, config).connected?
         else
           connected_without_proxy?
         end
